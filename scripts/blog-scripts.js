fdce8ea3934f05ed98ef1adb4a8268501be909f6
dschlessinger <schlessinger.daniel@gmail.com>
2014-08-26 16:00:39 -0400

    updates

--- scripts/blog-scripts.js
+++ scripts/blog-scripts.js
@@ -46,7 +46,7 @@ $.fn.scrollFun = function () {
 		var h = $(this).attr('href'),
 			target;
 
-		if (h.charAt(0) == '#' && h.length > 1 && (target = $(h)).length > 0) {
+		if (h.charAt(0) == '#') {
 			var pos = Math.max(target.offset().top, 0);
 			e.preventDefault();
 			$('body,html').animate({
