1bc9778ad71f4664fdf43a99f40babbd372c68c6
dschlessinger <schlessinger.daniel@gmail.com>
2014-08-26 03:24:13 -0400

    major update to all blog pages

---
+++ scripts/blog-scripts.js
@@ -0,0 +1,58 @@
+$(window).load(function(){
+    var $container = $('.blogContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $('.blogFilter a').click(function(){
+        $('.blogFilter .current').removeClass('current');
+        $(this).addClass('current');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    });
+});
+
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+				scrollTop: target.offset().top - 40
+				}, 600);
+				return false;
+			}
+		}
+	});
+});
+
+$.fn.scrollFun = function () {
+	$(this).click(function (e) {
+		var h = $(this).attr('href'),
+			target;
+
+		if (h.charAt(0) == '#' && h.length > 1 && (target = $(h)).length > 0) {
+			var pos = Math.max(target.offset().top, 0);
+			e.preventDefault();
+			$('body,html').animate({
+				scrollTop: pos
+			}, 'slow', 'swing');
+		}
+	});
+};
+$('.scroll').scrollFun();
